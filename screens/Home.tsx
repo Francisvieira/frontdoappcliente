d7fbc434c5cd71ff217391d5f4117506de90bbc7
francisvieira <fvieira340@gmail.com>
2021-11-29 11:57:27 -0300

    verificar token

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,10 +1,33 @@
 import * as React from 'react'
-import { View,Text } from 'react-native' 
+import { View, Text } from 'react-native'
+import { servidor } from '../config/Path';
 
-export default function Home(){
-    return(
-    <View>
-     <Text>tela Home</Text>
-     </View>
+export default function Home({ route }) {
+
+    const { dados } = route.params;
+    console.log(`Dados na Home ->${dados[2]}`);
+
+    React.useEffect(() => {
+
+        fetch(`${servidor}`,{
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                'content-type': 'application/json',
+                'token': dados[2]
+            }
+        })
+            .then((response) =>response.json())
+                .then((result) => {
+                    console.log(result)
+                })
+                .catch((erro) => console.log(`Erro ao ler a api-> ${erro}`))
+
+    }, [])
+
+    return (
+        <View>
+            <Text>tela Home</Text>
+        </View>
     );
 }
--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,28 +1,70 @@
 import * as React from 'react'
 import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native'
 import { styles } from '../css/Styles';
-import { servidor } from '../config/path';
-export default function Login({navigation}) {
+import { servidor } from '../config/Path';
+
+let us = "";
+let sh = "";
+let resultado: any = [];
+
+export default function Login({ navigation }) {
+    const [usuario, setUsuario] = React.useState("");
+    const [senha, setSenha] = React.useState("");
+
     return (
         <View style={styles.container}>
-            <Image source={require("../assets/icon.png")} style={styles.imglogo}/>
+            <Image source={require("../assets/icon.png")} style={styles.imglogo} />
             <View style={styles.controles}>
-                <TextInput placeholder="Usuario" style={styles.input}/>
-                <TextInput placeholder="Senha" style={styles.input} secureTextEntry/>
+                <TextInput placeholder="Usuario" style={styles.input} value={usuario} onChangeText={(value) => setUsuario(value)} />
+                <TextInput placeholder="Senha" style={styles.input} secureTextEntry value={senha} onChangeText={(value) => setSenha(value)} />
 
-                <TouchableOpacity style={styles.btnlogar} onPress={()=>{
-                    navigation.navigate("Home");
-                }}>
+                <TouchableOpacity style={styles.btnlogar} onPress={() => {
+
+                    us = usuario;
+                    sh = senha;
+                    let retorno = efetuarLogin();
+
+                    if (retorno[0] == "logado") {
+                        navigation.navigate("Home",{dados:retorno})
+                    }
+                }}
+                >
                     <Text style={styles.txtbtnlogar}>Logar</Text>
                 </TouchableOpacity>
             </View>
 
-            <TouchableOpacity style={styles.btncadastrar} onPress={()=>{
+
+            <TouchableOpacity style={styles.btncadastrar} onPress={() => {
                 navigation.navigate("Cadastro");
             }}>
-                    <Text  style={styles.txtbtncadastrar}>Cadastrar</Text>
-                </TouchableOpacity>
+                <Text style={styles.txtbtncadastrar}>Cadastrar</Text>
+            </TouchableOpacity>
 
         </View>
     );
+}
+
+
+function efetuarLogin() {
+    fetch(`${servidor}/login`, {
+        method: "POST",
+        headers: {
+            accept: "application/json",
+            "content-type": "application/json"
+        },
+        body: JSON.stringify({
+            usuario: us,
+            senha: sh
+        })
+
+    })
+        .then((response) => response.json())
+        .then((rs) => {
+            console.log(rs);
+            resultado[0] = rs.output;
+            resultado[1] = rs.payload;
+            resultado[2] = rs.token;
+        })
+        .catch((erro) => console.error(`Erro ao tentar buscar a api ->${erro}`));
+    return resultado;
 }
